572bf1add51363293ae65c6411ee913eb15259da
swkim <swkim@433.co.kr>
2017-05-17 18:15:08 +0900

    first react

---
+++ src/components/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            name : ""
+        };
+    }
+    render(){
+        return (
+            <div>
+                <button onClick={() =>
+                    {this.setState({name:"swkim"});}}>
+                        Click Me</button>
+                <h1>Hello React!!! {this.state.name}</h1>
+            </div>
+        );
+    }
+}
+
+export default App;
