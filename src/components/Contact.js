595b842b2798ecc577a90ad75d01cc0ac1e6b36c
swkim <swkim@433.co.kr>
2017-05-22 18:17:59 +0900

    feature: redux 강의 추가

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -39,6 +39,23 @@ export default class Contact extends React.Component {
         this.handleEdit = this.handleEdit.bind(this);
     }
 
+    componentWillMount() {
+        const contactData = localStorage.contactData;
+        console.log("componentWillMount");
+        if(contactData) {
+            this.setState({
+                contactData : JSON.parse(contactData)
+            })
+        }
+    }
+
+    componentDidUpdate(prevProps, prevState) {
+        console.log("conponentDidUpdate");
+        if(JSON.stringify(prevState.contactData) != JSON.stringify(this.state.contactData)) {
+            localStorage.contactData = JSON.stringify(this.state.contactData);
+        }
+    }
+
     handleClick(key) {
         this.setState({
             selectedKey: key
